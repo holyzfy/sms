d5b6907d9c15ae957e4b2a1d9756ba7404e2193d
zhaofuyun <zhaofuyun202@gmail.com>
2019-07-11 11:14:26 +0800

    feat: 增加发送频率限制

--- config/default.js
+++ config/default.js
@@ -4,6 +4,7 @@ module.exports = {
         // see: https://github.com/NodeRedis/node_redis#options-object-properties 
     },
     expireMinutes: 10,
+    maxCountInOneMinute: 2,
     sms: {
         // see: https://github.com/qcloudsms/qcloudsms_js#%E7%A4%BA%E4%BE%8B 
         appId: '',
